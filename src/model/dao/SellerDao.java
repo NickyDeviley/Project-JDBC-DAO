3392c882f1c2501c398db0c67688e11e8bba4b6d
Nicholas <Nickmilanin@gmail.com>
2026-08-08 07:04:39 -0300

    findByDepartment implementation

--- src/model/dao/SellerDao.java
+++ src/model/dao/SellerDao.java
@@ -2,14 +2,16 @@ package model.dao;
 
 import java.util.List;
 
+import model.entities.Department;
 import model.entities.Seller;
 
 public interface SellerDao {
 	
-	void insert(Seller obj);	// Método que vai adicionar um department ao banco de dados
-	void update(Seller obj);	// Método que atualiza um department do banco de dados
-	void deleteById(int id);	// Método que deleta um department do banco de dados baseado no ID
-	Seller findById(int id);	// Método que retorna um department do banco de dados baseado no ID
-	List<Seller> findAll();		// Retorna uma lista com todos os department
+	void insert(Seller obj);								// Método que vai adicionar um department ao banco de dados
+	void update(Seller obj);								// Método que atualiza um department do banco de dados
+	void deleteById(int id);								// Método que deleta um department do banco de dados baseado no ID
+	Seller findById(int id);								// Método que retorna um department do banco de dados baseado no ID
+	List<Seller> findAll();									// Retorna uma lista com todos os department
+	List<Seller> findByDepartment(Department department);	// Retorna uma lista com todos os sellers de um departamento específico
 	
 }
---
+++ src/model/dao/impl/DepartmentDaoJDBC.java
@@ -0,0 +1,40 @@
+package model.dao.impl;
+
+import java.util.List;
+
+import model.dao.DeparmentDao;
+import model.entities.Department;
+
+public class DepartmentDaoJDBC implements DeparmentDao {
+
+	@Override
+	public void insert(Department obj) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void update(Department obj) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void deleteById(int id) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Department findById(int id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Department> findAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
